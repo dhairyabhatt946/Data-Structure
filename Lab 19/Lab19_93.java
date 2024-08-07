a9d27b8dbc27f1f646e13bae52e2e52483522cd9
Dhairya Bhatt <143011610+dhairyabhatt946@users.noreply.github.com>
2024-08-07 19:51:00 +0530

    Add files via upload

---
+++ Lab 19/Lab19_93.java
@@ -0,0 +1,27 @@
+public class Lab19_93 {
+    public static void main(String[] args) {
+        int[] arr = {10,9,8,7,6,5,4,3,2,1};
+        
+        insertion_sort(arr);
+        for(int temp: arr) {
+            System.out.print(temp + ", ");
+        }
+    }
+
+    public static void insertion_sort(int[] arr) {
+        if(arr.length == 1) return;
+
+        int i = 1;
+        while(i < arr.length) {
+            int key = arr[i];
+            int j = i - 1;
+            while(j >= 0 && arr[j] > key) {
+                int temp = arr[j];
+                arr[j] = arr[j+1];
+                arr[j+1] = temp;
+                j--;
+            }
+            i++;
+        }
+    }
+}
